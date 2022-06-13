35f7b303294fa7950cf209572d260d3dffda82ed
lowkeyerWang <fwangjzq@gmail.com>
2022-06-13 22:20:17 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,18 @@
+let mic; 
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth , windowHeight);
+  xpos = width * 0.5;
+  ypos = height * 0.5;
+  mic  = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill("red");
-    noStroke();
-    ellipse(mouseX, mouseY, 60, 60);
-  }
+  background(40);
+  const vol = mic.getLevel();
+  const radius = map(vol, 0, 0.05, 100, 500);
+  noStroke();
+  fill(200, 190, 190);
+  ellipse(xpos, ypos, radius, radius);
 }
