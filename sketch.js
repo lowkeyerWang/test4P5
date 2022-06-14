b1a1ae15c000aa8f116b80619187a86e1123922c
lowkeyerWang <fwangjzq@gmail.com>
2022-06-14 10:25:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
+let penState = 1;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill("red");
-    noStroke();
-    ellipse(mouseX, mouseY, 60, 60);
+    if (penStats == 1) {
+      strokeWeight(10);
+      stroke(10);
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+    if (penStats == 2) {
+      fill("red");
+      noStroke();
+      ellipse(mouseX, mouseY, 60, 60);
+    }
+  }
+}
+
+function keyTyped() {
+  if (key == 'e') {
+    penState = 2;
+  }
+  if (key == 'r') {
+    penState = 1;
   }
 }
